bd1b096d614b00abe1ba3b7a5dfc77b597931ebf
Leandro Galante <lgalante@ezconet.com.br>
2021-12-07 00:00:21 -0300

    Initial

---
+++ MarsNavigatorRobot.Test/MarsTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace MarsNavigatorRobot.Test
+{
+    [TestFixture]
+    class MarsTests
+    {
+        [Test]
+        public void CreateMarsAsAAGridSuccess()
+        {
+
+        }
+    }
+}
---
+++ MarsNavigatorRobot.Test/RobotTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace MarsNavigatorRobot.Test
+{
+    [TestFixture]
+    class RobotTests
+    {
+        [Test]
+        public void RobotNavigateOnMarsSuccess()
+        {
+        }
+
+        [Test]
+        public void InputSeriesOfCommandsSuccess()
+        {
+
+        }
+
+        [Test]
+        public void RobotAlwaysStartAtX1Y1FacingNorthSuccess()
+        {
+
+        }
+
+
+    }
+}
