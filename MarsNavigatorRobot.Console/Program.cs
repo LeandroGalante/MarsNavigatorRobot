8403d966b3ef36b5c0ab24a86de021968930b3fa
LeandroGalante <leandrodasilvagalante@gmail.com>
2021-12-07 11:57:07 -0300

    Add Console

---
+++ MarsNavigatorRobot.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using MarsNavigatorRobot.Compass;
+using MarsNavigatorRobot.Plateau;
+using MarsNavigatorRobot.Position;
+using MarsNavigatorRobot.Robot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsNavigatorRobot.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.
+
+            var currentPosition = new CurrentPosition();
+            var marsPlateu = new MarsPlateau("5x5");
+            var compassNavigator = new CompassNavigator();
+            var positionNavigator = new PositionNavigator(currentPosition, marsPlateu, compassNavigator);
+            var robot = new MarsRobot("FFRFLFLF", currentPosition, positionNavigator);
+
+            
+        }
+    }
+}
---
+++ MarsNavigatorRobot.UI/Program.cs
@@ -0,0 +1,36 @@
+﻿using MarsNavigatorRobot.Compass;
+using MarsNavigatorRobot.Plateau;
+using MarsNavigatorRobot.Position;
+using MarsNavigatorRobot.Robot;
+using System;
+
+namespace MarsNavigatorRobot.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input1: Mars plateau is a grid such as 5x5, 3x4, etc.");
+            Console.WriteLine("Input2: The Command to move the Robot. Sample command string: LFLRFLFF");
+
+            Console.WriteLine("\nEnter Input1:");
+            var grid = Console.ReadLine();
+
+            Console.WriteLine("\nEnter Input2:");
+            var command = Console.ReadLine();
+
+            var currentPosition = new CurrentPosition();
+            var marsPlateu = new MarsPlateau(grid);
+            var compassNavigator = new CompassNavigator();
+            var positionNavigator = new PositionNavigator(currentPosition, marsPlateu, compassNavigator);
+            var robot = new MarsRobot(command, currentPosition, positionNavigator);
+
+            //Act
+            robot.Navigate();
+            Console.WriteLine($"\nResult: {robot.GetCurrentPosition()}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
